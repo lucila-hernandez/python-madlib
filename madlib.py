c48f41f59ba178ebeebc003f9e8fb5c0894c1130
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2024-09-11 14:30:54 -0700

    Added file via upload

---
+++ madlib.py
@@ -0,0 +1,91 @@
+
+# Homework: Create a madlib. Imagine a story where some of the words are 
+# supplied by user input. Using python you will use input to collect 
+# words for a story and then display the story. 
+
+# Use input to collect each word to a variable
+# Use an f string to display the story
+
+# Your madlib must collect at least 6 words!
+
+# print(f"My {noun} wore {color} shoes while they counted to {num}")
+
+# print(f"{name} wore {color} shoes while they counted to {num}")
+
+
+print("Welcome to todays Madlib!")
+
+movie_name = input("Please enter a movie name: ")
+first_adjective = input("Please enter an adjective: ")
+second_adjective = input("Please enter an adjective: ")
+number = input("Please enter a number: ")
+verb = input("Please enter a verb: ")
+second_verb = input("Please enter a verb: ")
+
+print(f" My favorite movie in the world is {movie_name}. It's so {first_adjective} and {second_adjective}. I watch it about {number} times a year. The funnest part is when they are {verb} and {second_verb}.")
+
+#My favorite movie in the world is __movie name__. It's so _addjective___ and _adjective__. I rewatch about _number___ times a yea. 
+#The funnest part is when they are_ verb_ and __verb_.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------
+# Partial solution
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = input("Name:")
+# color = input("color:")
+# num = input("Number:")
+
+# print(f"{name} wore {color} shoes while they counted to {num}")
